61780cdf1fda88db0641643a452590ecd31ffc4b
Aaron Hardy <aahardy@adobe.com>
2015-11-18 19:19:51 -0700

    Fixed push module to not run any logic unless task is run.

--- tasks/push.js
+++ tasks/push.js
@@ -5,34 +5,33 @@ var GulpSSH = require('gulp-ssh');
 var through = require('through2');
 var os = require('os');
 
-
-var packageData = require(path.join(__dirname, '../package.json'));
-var version = packageData.version;
-
-var config = {
-  host: 'adobetag.upload.akamai.com',
-  port: 22,
-  username: 'sshacs',
-  privateKey: fs.readFileSync(path.join(os.homedir(), '.ssh/dtm.akamai.prod')),
-  basePath: '/126057/activation/reactor/iframe/'
-};
-
-var gulpSSH = new GulpSSH({
-  ignoreErrors: false,
-  sshConfig: config
-});
-
-function checkVersionExists(callback) {
-  gulpSSH
-    .exec(['ls -a ../activation/reactor/iframe/' + version])
-    .pipe(through.obj(function (chunk, enc, cb) {
-      callback(!(/No such file or directory/.test(String(chunk.contents))));
-      cb();
-    }));
-}
-
 module.exports = function(gulp) {
   gulp.task('windgoggles:iframePush', ['windgoggles:minify'], function (callback) {
+    var packageData = require(path.join(__dirname, '../package.json'));
+    var version = packageData.version;
+
+    var config = {
+      host: 'adobetag.upload.akamai.com',
+      port: 22,
+      username: 'sshacs',
+      privateKey: fs.readFileSync(path.join(os.homedir(), '.ssh/dtm.akamai.prod')),
+      basePath: '/126057/activation/reactor/iframe/'
+    };
+
+    var gulpSSH = new GulpSSH({
+      ignoreErrors: false,
+      sshConfig: config
+    });
+
+    function checkVersionExists(callback) {
+      gulpSSH
+        .exec(['ls -a ../activation/reactor/iframe/' + version])
+        .pipe(through.obj(function (chunk, enc, cb) {
+          callback(!(/No such file or directory/.test(String(chunk.contents))));
+          cb();
+        }));
+    }
+
     checkVersionExists(function(versionFound) {
       if (versionFound) {
         console.error('Version `' + version + '` is already present on CDN.');

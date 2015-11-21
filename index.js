b4d5e59f723faf214a1e0c58913cfb6cdabd06ef
Jeff Hicken <hicken@hicken-osx.corp.adobe.com>
2015-11-20 17:14:32 -0700

    removing a bunch of un needed code and using the bridge object as the base to work from.

--- index.js
+++ index.js
@@ -1,38 +1,16 @@
-var extensionBridge = require('lens-extension-bridge/src/extensionBridge')();
+var extensionBridge = require('lens-extension-bridge/src/extensionBridge');
 var iframeResizer = require('iframe-resizer').iframeResizer;
 var frameboyant = require('frameboyant/frameboyant')();
 
-var WindGoggles = function(iframe) {
-  this.iframe = iframe;
-};
-
-WindGoggles.prototype = {
-  initialize: function() {
-    frameboyant.addIframe(this.iframe);
-    iframeResizer({checkOrigin: false}, this.iframe);
-  },
-
-  destroy: function() {
-    frameboyant.removeIframe(this.iframe);
-    this.iframe.iFrameResizer.close();
-  },
-
-  validate: function(schema, callback) {
-    return extensionBridge.validate(this.iframe, schema, callback);
-  },
-
-  getConfig: function(callback) {
-    return extensionBridge.getConfig(this.iframe, callback);
-  },
-
-  setConfig: function(config) {
-    return extensionBridge.setConfig(this.iframe, config);
-  }
-};
-
 module.exports = function(iframe) {
-  var windGogglesFrame = new WindGoggles(iframe);
-  windGogglesFrame.initialize();
+  var equippedGoggles = extensionBridge(iframe);
+  frameboyant.addIframe(iframe);
+  iframeResizer({checkOrigin: false}, iframe);
 
-  return windGogglesFrame;
+  equippedGoggles.destroy = function() {
+    frameboyant.removeIframe(iframe);
+    iframe.iFrameResizer.close();
+  };
+
+  return equippedGoggles;
 };

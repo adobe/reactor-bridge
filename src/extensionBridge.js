5697b009d5ec8c217d2f411287ab4d8d4627584e
mohicken <mhicken@adobe.com>
2016-06-20 13:03:05 -0600

    Refactoring to contain more internal functionality for configuration changes (work in progress)

--- src/extensionBridge.js
+++ src/extensionBridge.js
@@ -5,54 +5,18 @@ var Channel = require('jschannel');
 var iframeResizer = require('iframe-resizer').iframeResizer;
 var frameboyant = require('@reactor/frameboyant/frameboyant');
 var createRenderCompleteState = require('./createRenderCompleteState');
+var cloneDeep = require('lodash.cloneDeep');
+var isEqual = require('lodash.isEqual');
 
-module.exports = function(iframe, options) {
-  if (iframe.destroyExtensionBridge) {
-    iframe.destroyExtensionBridge();
-  }
-
-  var renderCompleteState = createRenderCompleteState(options.onInitialRenderComplete);
+module.exports = function(iframe) {
+  if (iframe.bridge) { return iframe; }
 
   var channel = Channel.build({
     window: iframe.contentWindow,
     origin: '*',
     scope: 'extensionBridge'
   });
-
   var channelSenders = getChannelSenders(channel);
-
-  attachChannelReceivers(channel, {
-    domReadyCallback: renderCompleteState.markDomReady,
-    openCodeEditor: function() {
-      if (options.openCodeEditor) {
-        options.openCodeEditor.apply(null, arguments);
-      } else {
-        console.error('You must define options.openCodeEditor');
-      }
-    },
-    openRegexTester: function() {
-      if (options.openRegexTester) {
-        options.openRegexTester.apply(null, arguments);
-      } else {
-        console.error('You must define options.openRegexTester');
-      }
-    },
-    openDataElementSelector: function() {
-      if (options.openDataElementSelector) {
-        options.openDataElementSelector.apply(null, arguments);
-      } else {
-        console.error('You must define options.openDataElementSelector');
-      }
-    }
-  });
-
-  frameboyant.addIframe(iframe, {
-    stylesAppliedCallback: renderCompleteState.markStylesReady
-  });
-
-  iframeResizer({
-    checkOrigin: false
-  }, iframe);
 
   var destroyExtensionBridge = function() {
     frameboyant.removeIframe(iframe);
@@ -61,13 +25,117 @@ module.exports = function(iframe, options) {
     iframe.iFrameResizer.close();
     channel.destroy();
   };
-
   iframe.destroyExtensionBridge = destroyExtensionBridge;
 
-  return {
-    init: channelSenders.init,
-    validate: channelSenders.validate,
-    getSettings: channelSenders.getSettings,
-    destroy: destroyExtensionBridge
+  iframe.bridge = {
+    api: {
+      init: channelSenders.init,
+      validate: channelSenders.validate,
+      getSettings: channelSenders.getSettings,
+      destroy: destroyExtensionBridge,
+      openCodeEditor: null,
+      openRegexTester: null,
+      openDataElementSelector: null,
+      onInitialRenderComplete: function() {}
+    },
+    configuration: {
+      src: '',
+      settings: {},
+      propertySettings: {},
+      schema: {},
+      extensionConfigurations: [{}]
+    }
+  }
+
+  var lastConfiguration = cloneDeep(iframe.bridge.configuration);
+
+  var validateConfigurationAndUpdateIframe = function() {
+    var configurationIsValid = configurationItemsAreValid();
+    var configurationHasChanged = !isEqual(lastConfiguration, iframe.bridge.configuration);
+
+    if(configurationHasChanged && configurationIsValid) {
+      var configuration = iframe.bridge.configuration;
+      if(iframe.src !== configuration.src) {
+        iframe.src = configuration.src;
+        attachReadyFunctions();
+      }
+
+      iframe.bridge.init({
+        settings: configuration.settings,
+        propertySettings: configuration.propertySettings,
+        schema: configuration.schema,
+        extensionConfigurations: configuration.extensionConfigurations
+      });
+    }
+  }
+
+  var configurationItemsAreValid = function() {
+    var keys = Object.keys(iframe.bridge.configuration);
+    keys.forEach(function(key, index) {
+      if (!iframe.bridge.configuration[key]) {
+        console.error('You must define iframe.configuration.' + key);
+        return false;
+      }
+    });
+    return true;
+  }
+
+  // generic getter/setter to trigger internal updates when configuration changes
+  var configurationItemGetterSetter = {
+    _configurationItem: {},
+    get () { return this._configurationItem; },
+    set (configurationItem) {
+      this._configurationItem = configurationItem;
+      validateConfigurationAndUpdateIframe();
+    }
   };
+
+  iframe.bridge.configurations = configurationItemGetterSetter;
+  iframe.bridge.configuration.src = configurationItemGetterSetter;
+  iframe.bridge.configuration.settings = configurationItemGetterSetter;
+  iframe.bridge.configuration.propertySettings = configurationItemGetterSetter;
+  iframe.bridge.configuration.schema = configurationItemGetterSetter;
+  iframe.bridge.configuration.extensionConfigurations = configurationItemGetterSetter;
+
+  var renderCompleteState = createRenderCompleteState(iframe.bridge.onInitialRenderComplete);
+
+
+  attachChannelReceivers(channel, {
+    openCodeEditor: function() {
+      if (iframe.bridge.openCodeEditor) {
+        iframe.bridge.openCodeEditor.apply(null, arguments);
+      } else {
+        console.error('You must define iframe.bridge.openCodeEditor');
+      }
+    },
+    openRegexTester: function() {
+      if (iframe.bridge.openRegexTester) {
+        iframe.bridge.openRegexTester.apply(null, arguments);
+      } else {
+        console.error('You must define iframe.bridge.openRegexTester');
+      }
+    },
+    openDataElementSelector: function() {
+      if (iframe.bridge.openDataElementSelector) {
+        iframe.bridge.openDataElementSelector.apply(null, arguments);
+      } else {
+        console.error('You must define iframe.bridge.openDataElementSelector');
+      }
+    }
+  });
+
+  var attachReadyFunctions = function() {
+    attachChannelReceivers(channel, {
+      domReadyCallback: renderCompleteState.markDomReady
+    });
+    frameboyant.addIframe(iframe, {
+      stylesAppliedCallback: renderCompleteState.markStylesReady
+    });
+  }
+
+  iframeResizer({
+    checkOrigin: false
+  }, iframe);
+
+  return iframe;
 };

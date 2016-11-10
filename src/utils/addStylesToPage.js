75dfac8ec978a33309e6e8b23c0a9e7cd1ad598b
Aaron Hardy <aahardy@adobe.com>
2016-11-10 15:17:27 -0700

    Made addStylesToPage "modern".

--- src/utils/addStylesToPage.js
+++ src/utils/addStylesToPage.js
@@ -3,14 +3,7 @@
  * @param {string} The CSS to be applied to the page.
  */
 export default css => {
-  var styleTag = document.createElement('style');
-
-  styleTag.type = 'text/css';
-  if (styleTag.styleSheet){
-    styleTag.styleSheet.cssText = css;
-  } else {
-    styleTag.appendChild(document.createTextNode(css));
-  }
-
-  (document.head || document.getElementsByTagName('head')[0]).appendChild(styleTag);
+  const styleTag = document.createElement('style');
+  styleTag.appendChild(document.createTextNode(css));
+  document.head.appendChild(styleTag);
 };
